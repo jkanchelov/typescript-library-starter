5ff9eb1ee257679543751f906e011026387f43b5
jkanchelov <yordan.kanchelov@gmail.com>
2021-01-02 18:49:47 +0200

    Transpile the umd module through Babel

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,37 +2,81 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import sourceMaps from "rollup-plugin-sourcemaps";
 import camelCase from "lodash.camelcase";
-import typescript from "rollup-plugin-typescript2";
 import json from "rollup-plugin-json";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
+import path from "path";
 
 const pkg = require("./package.json");
 
 const libraryName = "--libraryname--";
 
-export default {
-    input: `src/${libraryName}.ts`,
-    output: [
-        { file: pkg.main, name: camelCase(libraryName), format: "umd", sourcemap: true },
-        { file: pkg.module, format: "es", sourcemap: true },
-    ],
-    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-    external: [],
-    watch: {
-        include: "src/**",
-    },
-    plugins: [
-        // Allow json resolution
-        json(),
-        // Compile TypeScript files
-        typescript({ useTsconfigDeclarationDir: true }),
-        // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-        commonjs(),
-        // Allow node_modules resolution, so you can use 'external' to control
-        // which external modules to include in the bundle
-        // https://github.com/rollup/rollup-plugin-node-resolve#usage
-        resolve(),
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
 
-        // Resolve source maps to the original source
-        sourceMaps(),
-    ],
-};
+export default [
+    {
+        input: `src/index.ts`,
+        output: [{ file: pkg.main, name: camelCase(libraryName), format: "umd", sourcemap: true }],
+        // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+        external: [],
+        watch: {
+            include: "src/**",
+        },
+        plugins: [
+            // Allow json resolution
+            json(),
+            // Compile TypeScript files
+            // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+            commonjs(),
+            // Allow node_modules resolution, so you can use 'external' to control
+            // which external modules to include in the bundle
+            // https://github.com/rollup/rollup-plugin-node-resolve#usage
+            resolve({ extensions }),
+
+            babel({
+                extensions,
+                babelHelpers: "bundled",
+                include: ["src/**/*"],
+            }),
+            // Resolve source maps to the original source
+            sourceMaps(),
+        ],
+    },
+    {
+        input: `src/index.ts`,
+        output: [
+            {
+                file: path.parse(pkg.main).dir + `/${path.parse(pkg.main).name}` + "-min.js",
+                name: camelCase(libraryName),
+                format: "umd",
+                sourcemap: true,
+            },
+        ],
+        // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+        external: [],
+        watch: {
+            include: "src/**",
+        },
+        plugins: [
+            // Allow json resolution
+            json(),
+            // Compile TypeScript files
+            // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+            commonjs(),
+            // Allow node_modules resolution, so you can use 'external' to control
+            // which external modules to include in the bundle
+            // https://github.com/rollup/rollup-plugin-node-resolve#usage
+            resolve({ extensions }),
+
+            babel({
+                extensions,
+                babelHelpers: "bundled",
+                include: ["src/**/*"],
+            }),
+            // Resolve source maps to the original source
+            sourceMaps(),
+
+            terser(),
+        ],
+    },
+];
